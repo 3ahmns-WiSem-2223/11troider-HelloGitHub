4e80d39d179a519282706c999d06375f14951dbf
theoroiroi <114597785+theoroiroi@users.noreply.github.com>
2022-09-28 17:36:47 +0200

    Add files via upload

---
+++ Assets/scripts/HelloWorldAlt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldAlt : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+}
